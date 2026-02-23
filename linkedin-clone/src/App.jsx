9d60c2876e81d69bc2f49b47dad9204ae0c85923
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-23 10:12:40 +0100

    First commit

---
+++ linkedin-clone/src/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "./App.css";
+
+function App() {
+  return <h1>Hola!</h1>;
+}
+
+export default App;
