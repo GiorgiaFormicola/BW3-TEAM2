a300d629f571fda0513561a6bbcbb4965b889c35
SergioMoschini <moschini2002@gmail.com>
2026-02-24 14:49:00 +0100

    aggiunto footer, activity ecc al file su locale

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -4,11 +4,12 @@ import "./App.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import ProfilePage from "./components/ProfilePage";
-
+import MyFooter from "./components/MyFooter";
 function App() {
   return (
     <Provider store={store}>
       <ProfilePage></ProfilePage>
+      <MyFooter></MyFooter>
     </Provider>
   );
 }
