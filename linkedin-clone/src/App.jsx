b443aa36d2d0feb8c659daedfbb17c5828832c67
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-24 10:37:10 +0100

    Remove import from App.jsx

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -1,10 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
-import ProfileMainSection from "./components/ProfileMainSection";
 
 function App() {
-  return <ProfileMainSection></ProfileMainSection>;
+  return;
 }
 
 export default App;
